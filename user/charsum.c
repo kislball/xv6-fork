ad43ffaa5474ac9fcab1ead08f92771eba901ddc
Georgiy Mikhailenko <kislball@yahoo.com>
2026-09-19 16:55:23 +0300

    Add charsum user program
    This program takes the first character of its two input arguments and
    adds their ASCII codes

---
+++ user/charsum.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  char a = 'A', b = 'B';
+  if (argc >= 2) a = argv[1][0];
+  if (argc == 3) {
+    b = argv[2][0];
+  }
+
+  // sanity check
+  int result = intsum(a, b);
+
+  exit(result);
+}
