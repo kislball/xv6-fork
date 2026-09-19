57a0ccac00cbafabee7badde1bcc00849435e466
Georgiy Mikhailenko <kislball@yahoo.com>
2026-09-19 17:44:17 +0300

    Format

--- user/task3.c
+++ user/task3.c
@@ -11,22 +11,22 @@ main(int argc, char *argv[])
   argvv[1] = 0;
   pipe(p);
   if (fork() == 0) {
-    close(0); 
-    dup(p[0]); 
-    close(p[0]); 
-    close(p[1]); 
+    close(0);
+    dup(p[0]);
+    close(p[0]);
+    close(p[1]);
     exec("/wc", argvv);
   } else {
-    close(p[0]); 
-    for (int i = 0; i < argc; i ++) { 
-      write(p[1], argv[i], strlen(argv[i])); 
-      if(i + 1 < argc){
+    close(p[0]);
+    for (int i = 0; i < argc; i++) {
+      write(p[1], argv[i], strlen(argv[i]));
+      if (i + 1 < argc) {
         write(p[1], " ", 1);
       } else {
         write(p[1], "\n", 1);
+      }
     }
-    }
-    close(p[1]); 
+    close(p[1]);
     wait(0);
   }
   exit(0);
