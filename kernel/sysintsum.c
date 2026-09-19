687662c0cde556255520321ab1ca9e2949cc04e5
Georgiy Mikhailenko <kislball@yahoo.com>
2026-09-19 16:58:20 +0300

    Formatting

--- kernel/sysintsum.c
+++ kernel/sysintsum.c
@@ -13,7 +13,8 @@ sys_intsum(void)
   argint(1, &b);
 
   struct proc *p = myproc();
-  printk("intsum: pid=%d %s, a=%d, b=%d, a+b=%d\n", p->pid, p->name, a, b, a + b);
+  printk("intsum: pid=%d %s, a=%d, b=%d, a+b=%d\n", p->pid, p->name, a, b,
+         a + b);
 
   return a + b;
 }
--- user/charsum.c
+++ user/charsum.c
@@ -6,7 +6,8 @@ int
 main(int argc, char *argv[])
 {
   char a = 'A', b = 'B';
-  if (argc >= 2) a = argv[1][0];
+  if (argc >= 2)
+    a = argv[1][0];
   if (argc == 3) {
     b = argv[2][0];
   }
