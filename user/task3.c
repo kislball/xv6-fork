b7f643ada5df1f0c02439de56179f309cea501db
Mikhail Romanovskiy <romanovskymv@gmail.com>
2026-09-19 17:26:26 +0300

    Add task3

---
+++ user/task3.c
@@ -0,0 +1,33 @@
+#include "kernel/types.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int p[2];
+  char *argvv[2];
+  argvv[0] = "wc";
+  argvv[1] = 0;
+  pipe(p);
+  if (fork() == 0) {
+    close(0); 
+    dup(p[0]); 
+    close(p[0]); 
+    close(p[1]); 
+    exec("/wc", argvv);
+  } else {
+    close(p[0]); 
+    for (int i = 0; i < argc; i ++) { 
+      write(p[1], argv[i], strlen(argv[i])); 
+      if(i + 1 < argc){
+        write(p[1], " ", 1);
+      } else {
+        write(p[1], "\n", 1);
+    }
+    }
+    close(p[1]); 
+    wait(0);
+  }
+  exit(0);
+}
